0b93da1599d76b8781b5f4bd1701aa2c85571377
Eldar Gabdullin <eldargab@gmail.com>
2017-03-24 11:32:44 +0300

    fix diff.mk escaping

--- check/diff.mk
+++ check/diff.mk
@@ -2,5 +2,5 @@
 out/check:
 	@mkdir -p out
 	@echo "#!/bin/bash" > out/check
-	@echo "diff $2 $3 --ignore-space" >> out/check
+	@echo "diff \"\$$2\" \"\$$3\" --ignore-space" >> out/check
 	@chmod +x out/check
